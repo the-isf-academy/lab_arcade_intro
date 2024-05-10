aa2c9e558d97bfd693a1a3c24bdb296869816b5f
Emma <eqbrown@isf.edu.hk>
2024-05-10 08:46:47 +0800

    adding properties, renaming files

--- game1.py
+++ game1.py
@@ -7,28 +7,6 @@
 
 import arcade
 
-TILE_SCALING = 0.5
-PLAYER_SCALING = 1
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Game Platformer Example"
-SPRITE_PIXEL_SIZE = 128
-GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
-
-# How many pixels to keep as a minimum margin between the character
-# and the edge of the screen.
-VIEWPORT_MARGIN_TOP = 60
-VIEWPORT_MARGIN_BOTTOM = 60
-VIEWPORT_RIGHT_MARGIN = 270
-VIEWPORT_LEFT_MARGIN = 270
-
-# Physics
-MOVEMENT_SPEED = 5
-JUMP_SPEED = 25
-GRAVITY = 1.1
-
-
 class MyGame(arcade.Window):
     """Main application class."""
 
@@ -36,10 +14,22 @@ class MyGame(arcade.Window):
         """
         Initializer
         """
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        self.screen_width = 400
+        self.screen_height = 200
+
+        super().__init__(
+            width = self.screen_width, 
+            height = self.screen_height, 
+            title = "Game 1")
+
+
+        # Physics
+        self.MOVEMENT_SPEED = 5
+        self.JUMP_SPEED = 25
+        self.GRAVITY = 1.1
 
         self.game_over = False
-        self.score = 0
         
      
         """Set up the game and initialize the variables."""
@@ -49,11 +39,11 @@ class MyGame(arcade.Window):
 
         self.player_sprite = arcade.Sprite(
             "assets/player_sprite.png",
-            PLAYER_SCALING,
+            scale = 1,
+            center_x = 196,
+            center_y = 270
         )
 
-        self.player_sprite.center_x = 196
-        self.player_sprite.center_y = 270
         self.player_list.append(self.player_sprite)
 
         # sets up map
@@ -64,12 +54,19 @@ class MyGame(arcade.Window):
             "Coins": {"use_spatial_hash": True},
         }
 
+        # map scaling
+        self.TILE_SCALING = 0.5
+        self.SPRITE_PIXEL_SIZE = 128
+        self.GRID_PIXEL_SIZE = self.SPRITE_PIXEL_SIZE * self.TILE_SCALING
+
         # read in the tiled map
         self.tile_map = arcade.load_tilemap(
-            map_name, layer_options=layer_options, scaling=TILE_SCALING
+            map_name, 
+            layer_options=layer_options,
+              scaling=self.TILE_SCALING
         )
 
-        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
+        self.end_of_map = self.tile_map.width * self.GRID_PIXEL_SIZE
 
         # sets wall and coin SpriteLists
         self.wall_list = self.tile_map.sprite_lists["Platforms"]
@@ -79,15 +76,15 @@ class MyGame(arcade.Window):
         # sets the background color
         arcade.set_background_color(arcade.color.BLIZZARD_BLUE)
 
-
         # Keep player from running through the wall_list layer
-        walls = [self.wall_list, ]
+        walls = [self.wall_list]
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite, walls, gravity_constant=GRAVITY
+            self.player_sprite, walls, gravity_constant=self.GRAVITY
         )
 
         # sets up camera 
-        self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.camera = arcade.Camera(self.screen_width, self.screen_height)
 
         # center camera on user
         self.pan_camera_to_user()
@@ -106,15 +103,17 @@ class MyGame(arcade.Window):
         self.player_list.draw()
         self.wall_list.draw()
         self.coin_list.draw()
+
   
         # draw game over text if condition met
         if self.game_over:
             arcade.draw_text(
-                "Game Over",
+                f"Game Over",
                 self.player_sprite.center_x + 50,
                 self.player_sprite.center_y + 100,
                 arcade.color.BLACK,
-                30,
+                width=300,
+                multiline=True,
             )
 
     def on_key_press(self, key, modifiers):
@@ -124,13 +123,13 @@ class MyGame(arcade.Window):
 
         if key == arcade.key.UP:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = JUMP_SPEED
+                self.player_sprite.change_y = self.JUMP_SPEED
 
         elif key == arcade.key.LEFT:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
+            self.player_sprite.change_x = -self.MOVEMENT_SPEED
 
         elif key == arcade.key.RIGHT:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+            self.player_sprite.change_x = self.MOVEMENT_SPEED
 
         elif key == arcade.key.ESCAPE:
             arcade.exit()
@@ -156,9 +155,9 @@ class MyGame(arcade.Window):
         coins_hit = arcade.check_for_collision_with_list(
             self.player_sprite, self.coin_list
         )
+
         for coin in coins_hit:
             coin.remove_from_sprite_lists()
-            self.score += 1
 
         # Pan to the user
         self.pan_camera_to_user(panning_fraction=0.12)
@@ -167,14 +166,14 @@ class MyGame(arcade.Window):
         """ Manage Scrolling """
 
         # This spot would center on the user
-        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
-        screen_center_y = self.player_sprite.center_y - (
-            self.camera.viewport_height / 5
-        )
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width/2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height/2)
+
         if screen_center_x < 0:
             screen_center_x = 0
         if screen_center_y < 0:
             screen_center_y = 0
+
         user_centered = screen_center_x, screen_center_y
 
         self.camera.move_to(user_centered, panning_fraction)
---
+++ game2.py
@@ -0,0 +1,168 @@
+############################################
+# adapted from Python Aracde Example Games
+#
+# 💻 Experiment with the settings, see what you discover!
+############################################
+
+import random
+import arcade
+import os
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        """ 
+        Initializer set up the game and initialize the variables. 
+        """
+
+        # Call the parent class initializer
+        self.screen_width = 800
+        self.screen_height = 800
+
+        super().__init__(
+            width = self.screen_width,
+            height = self.screen_height, 
+            title = "Game Bullets Example")
+
+        # sets the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        # sets up the player 
+        self.player_list = arcade.SpriteList()
+
+        self.player_sprite = arcade.Sprite(
+            filename = "assets/player_sprite.png", 
+            center_x = 50,
+            center_y = 70,
+            scale = 0.5)
+        
+        self.movement_speed = 5
+
+        self.player_list.append(self.player_sprite)
+
+        # sets up the coins
+        self.coin_list = arcade.SpriteList()
+
+        for i in range(5):
+
+            # Create the coin instance
+            coin = arcade.Sprite(
+                filename = "assets/coin_sprite.png", 
+                scale =  0.005)
+
+            # Position the coin
+            coin.center_x = random.randrange(self.screen_width)
+            coin.center_y = 500
+
+            # Add the coin to the list
+            self.coin_list.append(coin)
+
+        # sets up lasers
+        self.laser_list = arcade.SpriteList()
+
+        self.laser_sprite_path = "assets/laser_sprite.png"
+        self.laser_sprite_scale = 0.03
+        self.laser_speed = 5
+
+        self.laser_sound = arcade.load_sound(path = "assets/laser-gun.wav")
+
+        # sets up score
+        self.score = 0
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        self.clear()
+
+        # Draw all the sprites.
+        self.player_list.draw()
+
+        self.coin_list.draw()
+        self.laser_list.draw()
+
+        # Render the text
+        arcade.draw_text(
+            f"Score: {self.score}", 
+            10, 
+            20, 
+            arcade.color.WHITE, 
+            14)
+    
+    def on_key_press(self, key, modifiers):
+        """
+        Called whenever a key is pressed.
+        """
+
+        if key == arcade.key.A:
+            self.player_sprite.change_x = -self.movement_speed
+
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = self.movement_speed
+
+        elif key == arcade.key.SPACE:
+            # Create a laser
+            laser = arcade.Sprite(
+                filename = self.laser_sprite_path,
+                scale = self.laser_sprite_scale)
+
+            # rotates image
+            laser.angle = 90
+
+            # Give the bullet a speed
+            laser.change_y = self.laser_speed
+
+            # Position the laser
+            laser.center_x = self.player_sprite.center_x
+            laser.bottom = self.player_sprite.top
+
+            # Add the laser to the appropriate sprite list
+            self.laser_list.append(laser)
+
+            arcade.play_sound(self.laser_sound)
+        
+        
+
+        elif key == arcade.key.ESCAPE:
+            arcade.exit()
+
+    def on_key_release(self, key, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        if key == arcade.key.A or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+        
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # updates player
+        self.player_list.update()
+
+        # updates on laser 
+        self.laser_list.update()
+
+        for laser in self.laser_list:
+
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(laser, self.coin_list)
+
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                laser.remove_from_sprite_lists()
+
+            # For every coin we hit, add to the score and remove the coin
+            for coin in hit_list:
+                self.score += 1
+
+            # If the bullet flies off-screen, remove it.
+            if laser.bottom > self.screen_height:
+                laser.remove_from_sprite_lists()
+
+
+if __name__ == "__main__":
+    window = MyGame()   
+    arcade.run()
